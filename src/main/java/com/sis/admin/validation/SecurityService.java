55b664bc4a4320a62cd811ef2551407d8973d457
Caner Özcan <caner.ozcan@ozu.edu.tr>
2026-05-09 13:34:39 +0300

    Implemented admin component controller, repositories, services and validators

--- src/main/java/com/sis/admin/validation/SecurityService.java
+++
@@ -1,8 +0,0 @@
-package com.sis.admin.validation;
-
-public class SecurityService {
-    public String hashPassword(String plainText) {
-
-        return "hashed_" + plainText; // Stubbed hash
-    }
-}
--- src/main/java/com/sis/admin/validation/UserValidator.java
+++
@@ -1,8 +0,0 @@
-package com.sis.admin.validation;
-
-public class UserValidator {
-    public boolean isUniqueEmail(String email) {
-
-        return true;
-    }
-}
